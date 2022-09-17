18c957c356dabcdca4a98b475bdfedc7c75e4792
jcuetos97 <105081149+jcuetos97@users.noreply.github.com>
2022-09-17 13:26:52 -0500

    Models updated

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/', withAuth, async (req,res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['name']],
+            });
+  
+        const users = userData.map((project) => project.get({ plain: true }));
+   
+        res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+    res.status(500).json(err);
+    } 
+});
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const dashbRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api');
 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashbRoutes);
 
 module.exports = router; 
